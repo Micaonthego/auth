41efd16aea1029e0f61f155bee49deff876c0067
github username <github email address>
2019-06-21 10:58:19 -0400

    Conditional logic added for what is rendered when logged in or out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+
+    state = {
+        loggedIn: null
+    }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyDt5xKdhosUdQl0CqWUwr7z5h2eBdZazPg',
@@ -15,14 +20,52 @@ export default class App extends Component {
             messagingSenderId: '815691498113',
             appId: '1:815691498113:web:90b3a1e8c5172e4e'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true })
+            } else {
+                this.setState({ loggedIn: false })
+            }
+        })
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <View style={styles.buttonContainerStyle}><Button onPress={() => firebase.auth().signOut()
+                    }>Log Out</Button>
+                    </View>
+                )
+            case false:
+                return <LoginForm />
+            default:
+                return <View style={styles.spinnerStyle}><Spinner size={"large"} /></View>
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
+    }
+}
+
+const styles = {
+    spinnerStyle: {
+        alignSelf: 'center'
+    },
+    buttonContainerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
     }
 }
